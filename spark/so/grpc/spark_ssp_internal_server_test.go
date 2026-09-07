598798a7de7daf987330afd29d232f3e35595712
benthecarman <benthecarman@live.com>
2026-09-07 17:48:39 -0500

    fixup! Add SSP deposit swaps and private leaf reads

--- spark/so/grpc/spark_ssp_internal_server_test.go
+++ spark/so/grpc/spark_ssp_internal_server_test.go
@@ -2,6 +2,13 @@ package grpc
 
 import (
 	"context"
+	"github.com/lightsparkdev/spark/common/keys"
+	"github.com/lightsparkdev/spark/so"
+	"github.com/lightsparkdev/spark/so/authn"
+	"github.com/lightsparkdev/spark/so/authz"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"strings"
 	"testing"
 
 	pbspark "github.com/lightsparkdev/spark/proto/spark"
@@ -38,4 +45,21 @@ func TestSparkSspInternalServerDelegatesToTreeCreationV2(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, createResponse)
 	require.Same(t, createRequest, handler.createRequest)
+}
+
+func TestPrivateWalletReadsRequireAllowedSSP(t *testing.T) {
+	server := NewSparkSspInternalServer(&so.Config{ServiceAuthz: so.ServiceAuthzConfig{Mode: authz.ModeEnforce}})
+	key := keys.GeneratePrivateKey().Public()
+	ctx := authn.InjectSessionForTests(t.Context(), key, 9999999999)
+	t.Setenv("SSP_INTERNAL_ALLOWED_IDENTITIES", "")
+	require.Equal(t, codes.Unauthenticated, status.Code(server.requireSSP(t.Context())))
+	require.Equal(t, codes.PermissionDenied, status.Code(server.requireSSP(ctx)))
+	_, err := server.QueryNodes(ctx, &pbspark.QueryNodesRequest{})
+	require.Equal(t, codes.PermissionDenied, status.Code(err))
+	_, err = server.QueryStaticDepositAddresses(ctx, &pbspark.QueryStaticDepositAddressesRequest{})
+	require.Equal(t, codes.PermissionDenied, status.Code(err))
+	t.Setenv("SSP_INTERNAL_ALLOWED_IDENTITIES", "other, "+strings.ToUpper(key.ToHex())+" ")
+	require.NoError(t, server.requireSSP(ctx))
+	other := authn.InjectSessionForTests(t.Context(), keys.GeneratePrivateKey().Public(), 9999999999)
+	require.Equal(t, codes.PermissionDenied, status.Code(server.requireSSP(other)))
 }
